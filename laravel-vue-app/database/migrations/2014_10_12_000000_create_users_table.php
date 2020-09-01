426ea88b6c0bf50744d5fe1413d4f40d742a97de
shoutee <hirashosanta@gmail.com>
2020-09-02 00:02:50 +0900

    データベースの取り込み

--- laravel-vue-app/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel-vue-app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->bigIncrements('user_id');
+            $table->string('name', 255)->unique();
+            $table->string('address', 255)->unique();
+            $table->timestamp('address_verified_at')->nullable();
+            $table->string('password', 100)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ laravel-vue-app/database/migrations/2020_08_31_222338_create_images_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->bigIncrements('image_id');
+            $table->bigInteger('note_id')->unsigned();
+            $table->foreign('note_id')->references('note_id')->on('notes');
+            $table->string('image_title', 100);
+            $table->string('file_name', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
