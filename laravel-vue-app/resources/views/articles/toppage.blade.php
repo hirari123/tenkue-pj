6d3295efd74e8dc0b6867ab6f1ce76c880ce6982
fuuuu <sanchan.fuu@gmail.com>
2020-10-15 07:34:27 +0900

    トップページの仕様変更

--- laravel-vue-app/resources/views/articles/toppage.blade.php
+++ laravel-vue-app/resources/views/articles/toppage.blade.php
@@ -14,43 +14,11 @@
     <form method="POST" action="{{ route('articles.store') }}" enctype="multipart/form-data">
 
         {{-- form.bladeの読み込み  --}}
-        @include('articles.form-new')
+        @include('articles.form-top')
 
-        {{-- ログイン時のみデータベースに登録する処理を行う --}}
-        @auth
-        <div class="top__btn">
-
-          <div class="top__btn-new">
-
-            {{-- noteの新規登録 --}}
-            <p>
-              <input type="submit" class="btn new" name="post-note" value="新規作成"/>
-            </p>
-
-            {{-- 画像登録 --}}
-            <p>
-              <input type="file" class="btn image" name="image" value="画像登録"/>
-            </p>
-
-          </div>
-
-
-          {{-- ログインしている時は一覧ボタンを表示する --}}
-          <div class="top__btn-login">
-
-              {{-- 一覧ページへの遷移ボタン --}}
-              <p>
-                  <a href="{{route('list')}}">
-                    <button type="button" class="btn">一覧</button>
-                </a>
-            </p>
-
-            </div>
-        </div>
-        @endauth
 
         {{-- 未ログイン時は下部にログインと会員登録のボタンを表示 --}}
-        {{-- ログインしていない時のノートの処理方法の検討 --}}
+
         @guest
         <div class="top__btn">
 
@@ -58,12 +26,12 @@
 
             {{-- noteの新規登録 --}}
             <p>
-              <input type="submit" class="btn new" name="post-note" value="新規作成"/>
+              <input type="submit" class="btn new" name="post-note" value="新規作成" disabled/>
             </p>
 
             {{-- 画像登録 未ログインユーザーでの実装方法を要検討 --}}
             <p>
-              <input type="submit" class="btn image" name="post-img" value="画像登録"/>
+              <input type="submit" class="btn image" name="post-img" value="画像登録" disabled/>
             </p>
 
           </div>
