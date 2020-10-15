7eb62611bfc8df74498011e484e05e42f2c4ef21
damgoweb <saito@damgoweb.com>
2020-10-15 23:46:13 +0800

    画面修正
    １.ログインボタン、パスワード再設定リンクを中央揃え　２.パスワード再設定の説明文記載

--- laravel-vue-app/resources/views/auth/passwords/email.blade.php
+++ laravel-vue-app/resources/views/auth/passwords/email.blade.php
@@ -5,29 +5,34 @@
 @section('title', 'パスワード再設定')
 
 @section('content')
-  <div class="container">
-    <div class="inner">
-      <header class="header">
-        <h1 class="apptitle">everynote</h1>
-      </header>
+<div class="container">
+  <div class="inner">
+    <header class="header">
+      <h1 class="apptitle">everynote</h1>
+    </header>
 
-      @include('error_card_list')
+    @include('error_card_list')
 
-      <div class="form_wrapper">
-        <form method="POST" action="{{ route('password.email') }}">
+    <div class="form_wrapper">
+      <form method="POST" action="{{ route('password.email') }}">
 
-          @csrf
+        @csrf
 
-          <p class="inputlabel">メールアドレス</p>
-          <p class="input"><input type="email" class="user" id=“email” name="email" placeholder="hoge@mail.com" required></p>
-          <div class="caution">
-            メール再設定の注意書き
-          </div>
+        <p class="inputlabel">メールアドレス</p>
+        <p class="input"><input type="email" class="user" id=“email” name="email" placeholder="hoge@mail.com" required></p>
+        <div class="caution">
+          登録されているメールアドレス宛にパスワード再設定のご案内メールをお送りします。<br><br>
 
-          <p class="btn"><input type="submit"  class="btn" name="mailsent" value="メール送信"></p>
+          メールに記載の手順に従ってパスワードを再設定してください。
+        </div>
 
-        </form>
-      </div><!--form_wrapper-->
-    </div><!--inner-->
-  </div><!--container--> 
+        <p class="btn"><input type="submit" class="btn" name="mailsent" value="メール送信"></p>
+
+      </form>
+    </div>
+    <!--form_wrapper-->
+  </div>
+  <!--inner-->
+</div>
+<!--container-->
 @endsection
